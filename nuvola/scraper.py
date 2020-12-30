4dea83d7f6ce716258a78c2f080e754df9f22eed
topongo <lorenzo.bodini.private@gmail.com>
2020-12-30 02:14:00 +0100

    Other fixes to scraper.py

--- nuvola/scraper.py
+++ nuvola/scraper.py
@@ -3,7 +3,7 @@ from requests import get
 from json.decoder import JSONDecodeError
 
 opt = seleniumwire.webdriver.ChromeOptions()
-opt.headless = True
+opt.headless = False
 
 
 class InvalidCredentialsException(Exception):
@@ -17,7 +17,7 @@ class ExpiredSessionTokenException(Exception):
 def scrape_from_token(session_token):
     try:
         return get("https://nuvola.madisoft.it/api-studente/v1/login-from-web",
-                   cookies={"Authorization": str(session_token)}).json()["token"]
+                   cookies={"nuvola": str(session_token)}).json()["token"]
     except JSONDecodeError:
         raise ExpiredSessionTokenException
 
@@ -27,13 +27,13 @@ def scrape_from_credentials(user, pwd):
     d.get("https://nuvola.madisoft.it/login/")
     d.find_element_by_id("username").send_keys(user)
     d.find_element_by_id("password").send_keys(pwd)
-    d.find_elements_by_tag_name("button")[0].click()
-    if d.current_url == "https://nuvola.madisoft.it/login":
+    d.find_elements_by_tag_name("button")[1].click()
+    if "https://nuvola.madisoft.it/area-studente" not in [i.url for i in d.requests]:
         raise InvalidCredentialsException
-    session_token = d.get_cookie("nuvola")
+    session_token = d.get_cookie("nuvola")["value"]
 
     try:
         return session_token, get("https://nuvola.madisoft.it/api-studente/v1/login-from-web",
-                                  cookie={"nuvola": str(session_token)}).json()["token"]
+                                  cookies={"nuvola": str(session_token)}).json()["token"]
     except JSONDecodeError:
         raise InvalidCredentialsException
